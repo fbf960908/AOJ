1c86f50654740f9b69b6632bc4709b54011a014e
BestFei <732847899@qq.com>
2017-04-24 23:02:10 +0800

    practice

--- src/AOJ/P1442船体强度.java
+++ src/AOJ/P1442船体强度.java
@@ -9,7 +9,22 @@ public class P1442´¬ÌåÇ¿¶È {
 	static PrintWriter cout = new PrintWriter(System.out);
 
 	public static void main(String[] args) {
-		
+		while(cin.hasNext()){
+			int N = cin.nextInt();
+			int T = cin.nextInt();
+			int ans = 0;
+			while(T-->0){
+				if(N<=1)
+					break;
+				N /= 2;
+				ans++;
+			}
+			if(N>1){
+				System.out.println("Impossible");
+			}
+			else
+				System.out.println(ans);
+		}
 		cin.close();
 		cout.close();
 	}
--- src/AOJ/P1443计树.java
+++ src/AOJ/P1443计树.java
@@ -9,7 +9,28 @@ public class P1443¼ÆÊ÷ {
 	static PrintWriter cout = new PrintWriter(System.out);
 
 	public static void main(String[] args) {
-		
+		while (cin.hasNext()) {
+			int a = cin.nextInt();
+			int sum = 0;
+			if (a % 2 == 0) {
+				System.out.println("0");
+				continue;
+			}
+			if (a == 3) {
+				System.out.println("1");
+				continue;
+			}
+			a = a / 2 - 1;
+			while (a > 0) {
+				int n = 1;
+				for (int i = 0; i < a; i++) {
+					n = n * 2 % 10007;
+				}
+				sum += n;
+				a -= 2;
+			}
+			System.out.println(sum);
+		}
 		cin.close();
 		cout.close();
 	}
