d8cd81b1680aa3f9f29e3c3e38984796d0053d7b
BestFei <732847899@qq.com>
2017-05-06 21:09:23 +0800

    practice

--- src/AOJ/Main.java
+++ src/AOJ/Main.java
@@ -1,12 +1,32 @@
 package AOJ;
 
 import java.util.Scanner;
+
 public class Main {
 
 	static Scanner cin = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+		int N = cin.nextInt();
+		int M = cin.nextInt();
+		int[] ss = new int[N + 1];
+		for (int i = 1; i < ss.length; i++) {
+			ss[i] = cin.nextInt();
+		}
+		for (int i = 0; i < M; i++) {
+			int sum = 0;
+			String str = cin.next();
+			int x = cin.nextInt();
+			int y = cin.nextInt();
+			if (str.equals("ADD")) {
+				ss[x] += y;
+			} else if (str.equals("QUERY")) {
+				for (int j = x; j <= y; j++) {
+					sum += ss[j];
+				}
+				System.out.println(sum);
+			}
+		}
 		cin.close();
 	}
 }
---
+++ src/AOJ/并查集.java
@@ -0,0 +1,52 @@
+package AOJ;
+
+import java.util.Scanner;
+
+public class ²¢²é¼¯ {
+
+	static Scanner cin = new Scanner(System.in);
+	static int[] father = new int[1000];
+	static int[] rank = new int[1000];
+
+	public static void main(String[] args) {
+		int N = cin.nextInt();
+		int M = cin.nextInt();
+		for (int i = 1; i <= N; i++) {
+			Make_set(i);
+		}
+		for (int i = 0; i < M; i++) {
+			int x = cin.nextInt();
+			int y = cin.nextInt();
+			union(x, y);
+		}
+		
+		cin.close();
+	}
+
+	static void Make_set(int x) {
+		father[x] = x;
+		rank[x] = 0;
+	}
+
+	static int find(int x) {
+		if (x != father[x]) {
+			father[x] = find(father[x]);
+		}
+		return father[x];
+	}
+
+	static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x == y)
+			return;
+		if (rank[x] > rank[y]) {
+			father[y] = x;
+		} else {
+			if (rank[x] == rank[y]) {
+				rank[y]++;
+			}
+			father[x] = y;
+		}
+	}
+}
